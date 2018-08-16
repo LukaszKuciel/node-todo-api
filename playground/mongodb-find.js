98c0af7c0ffe57cca5da22513affb6a145276966
Łukasz Kuciel <lukaszkuciel95@gmail.com>
2018-08-16 22:59:45 +0200

    Init repo

---
+++ playground/mongodb-find.js
@@ -0,0 +1,31 @@
+// const MongoClient = require('mongodb').MongoClient
+const {MongoClient, ObjectID} = require('mongodb')
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+    if(err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp')
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID("5b74ac70bd39491390c5eef9")
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, null, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // })
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to count todos', err);
+    // })
+
+    db.collection('User').find({name: 'Jane Doe'}).toArray().then((docs) => {
+        console.log(JSON.stringify(docs, null, 2));
+    })
+    client.close();
+});
