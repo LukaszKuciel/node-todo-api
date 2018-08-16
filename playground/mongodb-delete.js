74aba071149f7e1b7accac93355eab3df0faead0
Łukasz Kuciel <lukaszkuciel95@gmail.com>
2018-08-16 23:19:59 +0200

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,36 @@
+// const MongoClient = require('mongodb').MongoClient
+const {MongoClient, ObjectID} = require('mongodb')
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+    if(err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp')
+
+    //delete multiple
+    // db.collection('Todos').deleteMany({text:'Eat lunch'}).then((res) => {
+    //     console.log(res);
+    // })
+
+    //delete single
+    // db.collection('Todos').deleteOne({text:'Eat lunch'}).then((res) => {
+    //     console.log(res);
+    // })
+
+    //delete single finded
+    // db.collection('Todos').findOneAndDelete({completed: false}).then((res) => {
+    //     console.log(res);
+    // })
+
+    db.collection('User').deleteMany({name:'Lukasz Kuciel'}).then((res) => {
+        console.log(res);
+    })
+
+    db.collection('User').findOneAndDelete({_id: new ObjectID('5b75e48863c4e142ec79b233')}).then((res) => {
+        console.log(res);
+    })
+
+    client.close();
+});
