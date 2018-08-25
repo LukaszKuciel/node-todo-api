b59963c45d7bb360fb6bbf79f890d756135a3f75
Łukasz Kuciel <35846271+LukaszKuciel@users.noreply.github.com>
2018-08-25 16:05:52 +0200

    Delete mongoose.js

--- server/db/mongoose.js
+++
@@ -1,8 +0,0 @@
-let mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-
-const uri = "mongodb://LukaszKuciel:SpSDLcbRAyM5HAU6@todoapi-shard-00-00-j1yyk.gcp.mongodb.net:27017,todoapi-shard-00-01-j1yyk.gcp.mongodb.net:27017,todoapi-shard-00-02-j1yyk.gcp.mongodb.net:27017/test?ssl=true&replicaSet=TodoApi-shard-0&authSource=admin&retryWrites=true";
-mongoose.connect(uri, { useNewUrlParser: true });
-
-module.exports = {mongoose};
