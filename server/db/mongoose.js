271d4f12c9bccc655c7cdaa465cc402046d60c89
Łukasz Kuciel <lukaszkuciel95@gmail.com>
2018-08-25 15:58:36 +0200

    gcloud deploy

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+
+const uri = "mongodb://LukaszKuciel:SpSDLcbRAyM5HAU6@todoapi-shard-00-00-j1yyk.gcp.mongodb.net:27017,todoapi-shard-00-01-j1yyk.gcp.mongodb.net:27017,todoapi-shard-00-02-j1yyk.gcp.mongodb.net:27017/test?ssl=true&replicaSet=TodoApi-shard-0&authSource=admin&retryWrites=true";
+mongoose.connect(uri, { useNewUrlParser: true });
 
 module.exports = {mongoose};
