e0a84885d11232751a9d760da08eeee4d43750bf
Łukasz Kuciel <lukaszkuciel95@gmail.com>
2018-08-25 18:01:05 +0200

    test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -90,3 +90,39 @@ describe('GET /todos/:id', () => {
             .end(done)
     })
 })
+
+describe('DELETE /todos/:id', () => {
+    it('should delete todo doc', (done) => {
+        let hexid = todos[0]._id.toHexString();
+        request(app)
+            .delete(`/todos/${hexid}`)
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todo._id).toBe(hexid)
+            })
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
+                Todo.findById(hexid).then(todo => {
+                    expect(todo).toNotExist();
+                    done();
+                }).catch(e => done(e))
+            })
+    })
+
+    it('should return 404 if todo not found', (done) => {
+        let hexId = new ObjectID().toHexString();
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(404)
+            .end(done)
+    })
+
+    it('should return 404 for non-object ids', (done) => {
+        request(app)
+            .delete(`/todos/123`)
+            .expect(404)
+            .end(done)
+    })
+})
