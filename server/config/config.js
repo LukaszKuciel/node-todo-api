0961e26def24c02086fd5d1bb5737e34fb26c645
Łukasz Kuciel <lukaszkuciel95@gmail.com>
2018-10-07 16:22:06 +0200

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,9 @@
 let env = process.env.NODE_ENV || 'development';
 
-if(env === 'development'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-}else if(env === 'test'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-}else if(env === 'production'){
-    process.env.MONGODB_URI = "mongodb://LukaszKuciel:SpSDLcbRAyM5HAU6@todoapi-shard-00-00-j1yyk.gcp.mongodb.net:27017,todoapi-shard-00-01-j1yyk.gcp.mongodb.net:27017,todoapi-shard-00-02-j1yyk.gcp.mongodb.net:27017/test?ssl=true&replicaSet=TodoApi-shard-0&authSource=admin&retryWrites=true";
+if(env === 'development' || env === 'test'){
+    let config = require('./config.json');
+    let envConfig = config[env];
+    Object.keys(envConfig).forEach(key => {
+        process.env[key] = envConfig[key];
+    })
 }
